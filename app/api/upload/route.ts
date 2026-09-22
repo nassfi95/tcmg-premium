6d0c938b7e160cafefa7f7e50cd8996475bd620b
nassfi95 <saifinassim563@gmail.com>
2026-09-22 09:27:57 +0200

    Update route.ts

--- app/api/upload/route.ts
+++ app/api/upload/route.ts
@@ -13,11 +13,12 @@ export async function POST(req: Request) {
     }
 
     const blob = await put(file.name, file, {
-      access: 'public',
-      token:
-        process.env.BLOB_READ_WRITE_TOKEN ??
-        process.env.GALLERY_BLOB_READ_WRITE_TOKEN,
-    });
+  access: 'public',
+  addRandomSuffix: true,
+  token:
+    process.env.BLOB_READ_WRITE_TOKEN ??
+    process.env.GALLERY_BLOB_READ_WRITE_TOKEN,
+});
 
     return NextResponse.json({ url: blob.url });
   } catch (e) {
